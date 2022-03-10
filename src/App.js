17776f5805be68e8875fc89311f014f7458b5c96
Tran Phuc Thoai <tranphucthoaiit@gmail.com>
2022-03-10 19:05:50 +0700

    update 10-03-22-19h05

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { React } from "react";
 import { Route, Routes } from "react-router-dom";
 import { IndexPage, SinglePage, ListPage } from "./features";
 import { Header, Footer, NotFound, Error } from "./components";
-import "./assets/scss/app.scss";
 
 function App() {
   return (
